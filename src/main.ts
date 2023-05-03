09b1866474966f4eecf565fdb41370c8073fcb08
EugeniBykovski <bykovskieugeni@mail.ru>
2023-05-03 21:08:59 +0200

    added swagger docs to the project

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as session from 'express-session';
 import * as passport from 'passport';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,16 @@ async function bootstrap() {
 
   app.use(passport.initialize());
   app.use(passport.session());
+
+  const config = new DocumentBuilder()
+    .setTitle('E-commerce store')
+    .setDescription('store documentation')
+    .setVersion('1.0.0')
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(3000);
 }
